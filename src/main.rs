62580f18db710e8f4471703652489ed2d28a3b03
Cartrius Phipps <cartriusphipps@gmail.com>
2025-04-06 19:55:40 -0500

    Add get_upload_url_handler and Test endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+use aws_sdk_s3::presigning::PresigningConfig;
 use axum::{routing::get, Router, extract::Query};
 use aws_config::BehaviorVersion;
 use aws_sdk_s3::Client as S3Client;
 use serde::Deserialize;
 use std::net::SocketAddr;
 use std::error::Error;
-
+use std::time::Duration;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    // Load env vars
+    dotenv::dotenv().ok();
+
     // Load AWS config
     let shared_config = aws_config::defaults(BehaviorVersion::latest()).load().await;
     let s3_client = S3Client::new(&shared_config);
@@ -30,4 +34,31 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .serve(app.into_make_service())
         .await?;
     Ok(())
+}
+
+// Define query params struct to parse ?key=<S3-object-key> from URL
+#[derive(Deserialize)]
+struct GetUploadUrlParams {
+    key: String,
+}
+
+// GET /get-upload-url?key=somepath
+async fn get_upload_url_handler(
+    Query(params): Query<GetUploadUrlParams>,
+    s3_client: S3Client
+) -> String {
+    // Read the bucket name from an environment variable
+    // Default to "my-photo-bucket", if it's not set
+    let bucket = std::env::var("UPLOAD_BUCKET").unwrap_or_else(|_| "my-photo-bucket".to_string());
+    let request = s3_client
+        .put_object()
+        .bucket(&bucket)
+        .key(&params.key);
+
+    // Generate a presigned request, valid for a default expriation of 15 min
+    let presigned_req = request
+        .presigned(PresigningConfig::expires_in(Duration::from_secs(60 * 15))
+        .expect("15 minutes")).await.unwrap();
+
+    presigned_req.uri().to_string()
 }
